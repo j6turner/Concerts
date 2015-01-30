6d85be0f6c51a694f0911f5c03b50ed19a43873c
J. TURNER <student@epicodus.com>
2015-01-30 10:55:36 -0800

    Add validation for Venue class/venue table.

--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -1,5 +1,12 @@
 require("spec_helper")
 
 describe(Venue) do
+
   it { should have_and_belong_to_many(:bands) }
+
+  it("validates presence of name") do
+    venue = Venue.new({:name => ""})
+    expect(venue.save()).to(eq(false))
+  end
+
 end
